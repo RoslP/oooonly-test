58a05a951cd23608975a6ba958560c34d602a46c
pavel99 <ght12345ght12345@gmail.com>
2025-11-10 02:00:53 +1000

    Фабрики и сидер для генерации данных

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,60 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Position;
+use App\Models\ComfortCategory;
+use App\Models\CarModel;
+use App\Models\Driver;
+use App\Models\Car;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $categories = collect([
+            'Первая',
+            'Вторая',
+            'Третья',
+            'Четвёртая'
+        ])->map(fn($name) => ComfortCategory::create(['name' => $name]));
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $positions = collect([
+            'Директор',
+            'Программист',
+            'Менеджер',
+            'Бухгалтер',
+        ])->map(fn($name) => Position::create(['name' => $name]));
+
+        $positions->each(function ($pos) use ($categories) {
+            switch ($pos->name) {
+                case 'Директор':
+                    $allowed = [4];
+                    break;
+
+                case 'Программист':
+                    $allowed = [3];
+                    break;
+
+                case 'Менеджер':
+                    $allowed = [2];
+                    break;
+
+                case 'Бухгалтер':
+                    $allowed = [1];
+                    break;
+            }
+
+            $pos->comfortCategories()->sync($allowed);
+        });
+
+        Driver::factory(10)->create();
+
+        CarModel::factory(10)->create();
+
+        Car::factory(20)->create();
+
+        User::factory(50)->create();
     }
 }
